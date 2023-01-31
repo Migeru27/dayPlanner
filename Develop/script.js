d72a654aedc11782fec4cc3b8c38b1f9a6288982
Miguel A <migueldude27@gmail.com>
2023-01-31 13:14:59 -0500

    added time features for the page

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,62 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+let time = dayjs();
+console.log(time);
+
+//used to display time on the top of the page
+// let todayDate = time.format('DD/MM/YYYY');
+// $('#currentDay').text(todayDate);
+
+$(function currentTime(){
+  //adding message gm if morning and ga if afternoon
+  let mess = time.format('MM');
+
+  if(toString(mess) <= '11'){
+    mess = "Good Morning \n";
+  }
+  else{
+    mess = "Good Afternoon \n";
+  }
+
+
+
+
+  //format the time and spaced the time 
+  let todayDate = time.format('MM/DD/YYYY');
+  //wwt == whats the time?
+  let wtt = time.format('HH:mm:ss'); 
+  let today = document.getElementById("currentDay");
+  today.innerText = mess + todayDate + wtt;
+
+    
+
+  
+});
+
+
+
+
+//grabs the id of each time slot
+//using id by 24hr time over 12hr
+let hourOne = document.getElementById("hour-9");
+let hourTwo = document.getElementById("hour-10");
+let hourThree = document.getElementById("hour-11");
+let hourFour = document.getElementById("hour-12");
+let hourFive = document.getElementById("hour-13");
+let hourSix = document.getElementById("hour-14");
+let hourSeven = document.getElementById("hour-15");
+let hourEight = document.getElementById("hour-16");
+let hourNine = document.getElementById("hour-17");
+
+
+
+
+
+
+
+$(function main() {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +75,7 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+
+  currentTime();
+
 });
