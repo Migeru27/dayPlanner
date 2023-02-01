fed91de3de162ff243f66029456cd18f9fd5810a
unknown <migueldude27@gmail.com>
2023-02-01 12:38:46 -0500

    modified js and added more code to html

--- Develop/script.js
+++ Develop/script.js
@@ -5,37 +5,6 @@
 let time = dayjs();
 let hour = time.format('HH');
 
-//used to display time on the top of the page
-// let todayDate = time.format('DD/MM/YYYY');
-// $('#currentDay').text(todayDate);
-
-function currentTime(){
-  
-  //adding message gm if morning and ga if afternoon
-  let mess = "";
-  
-  if(toString(hour) <= '11'){
-    mess = "Good Morning \n";
-  }
-  else{
-    mess = "Good Afternoon \n";
-  }
-  //format the time and spaced the time 
-  var todayDate = time.format('MMM DD, YYYY');
-  //wwt == whats the time?
-  
-  var wtt = time.format('HH:mm:ss'); 
-  var today = document.getElementById("currentDay");
-  today.innerText = mess + todayDate +'\n'+ wtt;
-
-  
-
-
-}
-
-setInterval(currentTime,1000);
-
-
 //grabs the id of each time slot
 //using id by 24hr time over 12hr
 let hourOne = document.getElementById("hour-9");
@@ -48,32 +17,133 @@ let hourSeven = document.getElementById("hour-15");
 let hourEight = document.getElementById("hour-16");
 let hourNine = document.getElementById("hour-17");
 
-
-
-
-
-
-// $(function() {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-block containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
+$(function currentTime(){
   
- 
-// });
+  //adding message gm if morning and ga if afternoon
+  let mess = "";
+  
+  if(hour < 12){
+    mess = "Good Morning \n";
+  }
+  else{
+    mess = "Good Afternoon \n";
+  }
+  //format the time and spaced the time 
+  var todayDate = time.format('MMM DD, YYYY');
+  //wwt == whats the time?
+  
+  var wtt = time.format('HH:mm'); 
+  var today = document.getElementById("currentDay");
+  today.innerText = mess + todayDate +'\n'+ wtt;
+
+  
+  //couldn't get updated time to work properly 
+});
+
+
+
+//save button function
+$(".saveBtn").on("click", function () {
+  let events = $(this).siblings(".description").val();
+  let hour = $(this).parents().attr("id");
+  localStorage.setItem(events, hour);
+});
+
+
+//save contents to local storage when opened again
+$(function savePage() {
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+  $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+  $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+  $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+  $("#hour-17 .description").val(localStorage.getItem("hour-17"));
+});
+
+/*changes the color depending on the time of day
+  red == past
+  grey == present
+  green == present
+
+*/
+$(function colorChange() {
+
+  if (hour > 9) {
+    $(hourOne).addClass("past");
+  } else if (hour == 9) {
+    $(hourOne).addClass("present");
+  } else {
+    $(hourOne).addClass("future");
+  }
+
+  if (hour > 10) {
+    $(hourTwo).addClass("past");
+  } else if (hour == 10) {
+    $(hourTwo).addClass("present");
+  } else {
+    $(hourTwo).addClass("future");
+  }
+
+  if (hour > 11) {
+    $(hourThree).addClass("past");
+  } else if (hour == 11) {
+    $(hourThree).addClass("present");
+  } else {
+    $(hourThree).addClass("future");
+  }
+
+  if (hour > 12) {
+    $(hourFour).addClass("past");
+  } else if (hour == 12) {
+    $(hourFour).addClass("present");
+  } else {
+    $(hourFour).addClass("future");
+  }
+
+  if (hour > 13) {
+    $(hourFive).addClass("past");
+  } else if (hour == 13) {
+    $(hourFive).addClass("present");
+  } else {
+    $(hourFive).addClass("future");
+  }
+
+  if (hour > 14) {
+    $(hourSix).addClass("past");
+  } else if (hour == 14) {
+    $(hourSix).addClass("present");
+  } else {
+    $(hourSix).addClass("future");
+  }
+
+  if (hour > 15) {
+    $(hourSeven).addClass("past");
+  } else if (hour == 15) {
+    $(hourSeven).addClass("present");
+  } else {
+    $(hourSeven).addClass("future");
+  }
+
+  if (hour > 16) {
+    $(hourEight).addClass("past");
+  } else if (hour == 16) {
+    $(hourEight).addClass("present");
+  } else {
+    $(hourEight).addClass("future");
+  }
+
+  if (hour > 17) {
+    $(hourNine).addClass("past");
+  } else if (hour == 17) {
+    $(hourNine).addClass("present");
+  } else {
+    $(hourNine).addClass("future");
+  }
+});
+
+
 
 
